fe78b6a909d74419b1fd5a7363dc5c033ca87d88
bhargysh <bsha492@aucklanduni.ac.nz>
2018-11-15 07:56:21 +0000

    Plays note according to white or black keys

--- piano_setup.py
+++ piano_setup.py
@@ -7,10 +7,7 @@ sender = udp_client.SimpleUDPClient('127.0.0.1', 4559)
 mc = minecraft.Minecraft.create()
 
 positions = mc.player.getTilePos()
-
-'''def bulldozer(x, y, z):
-    mc.setBlocks(x - 30, y, z - 30, x + 30, y + 30, z + 30, 0)
-bulldozer(positions.x, positions.y, positions.z)'''
+print(positions)
 
 def make_octave(x, y, z):
     mc.setBlocks(x - 30, y, z - 30, x + 30, y + 30, z + 30, 0)
@@ -28,10 +25,33 @@ def white_key(x, y, z):
     mc.setBlocks(x, y - 1, z, x + 2, y - 1, z + 14, 44, 7)
 
 def centre_steve():
-    mc.player.setPos(positions.x + 8, positions.y + 3, positions.z + 12)
+    mc.player.setPos(positions.x + 8, positions.y, positions.z + 12)
 
 make_octave(positions.x, positions.y, positions.z)
 centre_steve()
-'''def play_note(note):
+
+white_notes = [60, 62, 64, 65, 67, 69, 71]
+black_notes = [61, 63, 0, 66, 68, 70]
+
+def play_note(note):
     sender.send_message('/play_this', note)
-    sleep(0.5)'''
+    sleep(0.5)
+
+while True:
+    new_pos = mc.player.getTilePos()
+    print(new_pos)
+    block_below = mc.getBlock(new_pos.x, new_pos.y - 1, new_pos.z)
+    print(block_below)
+
+    if block_below != 44 and block_below != 49:
+        block_below = mc.getBlock(new_pos.x, new_pos.y, new_pos.z)
+
+    relative_pos = positions.x - new_pos.x
+
+    if block_below == 44:
+        notes_across = relative_pos // -3
+        play_note(white_notes[notes_across])
+
+    if block_below == 49:
+        notes_across = ((relative_pos - 1) // -3) - 1
+        play_note(black_notes[notes_across])
